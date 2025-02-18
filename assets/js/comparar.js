97a3a0cf0d7bb01c7a72f5ea97cc87d6e51fa9de
Fernanda Santiago <fernandamaria5501@gmail.com>
2025-02-18 10:12:25 -0300

    ADD: adicionando os campos de texto ao js

---
+++ assets/js/comparar.js
@@ -0,0 +1,13 @@
+const cod = document.getElementById('codigo1').value;
+const cidade = document.getElementById('cidade1').value;
+const ano = document.getElementById('anoConstrucao1').value;
+const cursos = document.getElementById('quantCursos1').value;
+const cod2 = document.getElementById('codigo2').value;
+const cidade2 = document.getElementById('cidade2').value;
+const ano2 = document.getElementById('anoConstrucao2').value;
+const cursos2 = document.getElementById('quantCursos2').value;
+
+const gerarRelatorio = document.getElementById('gerarRelatorio');
+const limparRelatorio = document.getElementById('limparRelatorio');
+
+
